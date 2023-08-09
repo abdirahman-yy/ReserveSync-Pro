5c4e579408c61588d12deb90b6ff31bca19309fb
Abdirahman Yasin <78784947+abdirahman-yy@users.noreply.github.com>
2023-08-09 19:22:14 +0000

    implemented table schemas

--- reservesync-pro/src/superbase/superbase-config.js
+++ reservesync-pro/src/superbase/superbase-config.js
@@ -4,3 +4,65 @@ const supabaseUrl = 'https://htecmmpivhbfpvzmpbbn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0ZWNtbXBpdmhiZnB2em1wYmJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE1NjA3ODgsImV4cCI6MjAwNzEzNjc4OH0.CZYL01YdwwRMXeiEiwBcAuvCOO-o2UO-JJvlnnmdzJQ';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Corporation Table
+const corporationTable = 'corporation';
+const corporationFields = {
+  id: 'uuid',
+  name: 'text',
+  // Add other fields specific to corporations
+};
+
+// Employee Table
+const employeeTable = 'employee';
+const employeeFields = {
+  id: 'uuid',
+  corporation_id: 'uuid references corporation(id)',
+  name: 'text',
+  // Add other fields specific to employees
+};
+
+// Resource Table
+const resourceTable = 'resource';
+const resourceFields = {
+  id: 'uuid',
+  corporation_id: 'uuid references corporation(id)',
+  type: 'text',
+  name: 'text',
+  // Add other fields specific to resources
+};
+
+// Reservation Table
+const reservationTable = 'reservation';
+const reservationFields = {
+  id: 'uuid',
+  employee_id: 'uuid references employee(id)',
+  resource_id: 'uuid references resource(id)',
+  start_time: 'timestamp',
+  end_time: 'timestamp',
+  // Add other fields specific to reservations
+};
+
+// ResourceCategory Table
+const resourceCategoryTable = 'resource_category';
+const resourceCategoryFields = {
+  id: 'uuid',
+  name: 'text',
+  // Add other fields specific to resource categories
+};
+
+// Edge Cases and Considerations
+// Overlapping Reservations: Implement logic to prevent overlapping reservations for the same resource.
+// Resource Availability: Design a mechanism to mark resources as temporarily unavailable.
+// Cancellation and Modification: Define rules for cancellations and modifications.
+// Resource Type Flexibility: Ensure extensibility of the "type" field for future needs.
+// Timezone Handling: Consider timezones for start_time and end_time fields.
+// Reporting and Analytics: Plan for generating reports on reservations and resource utilization.
+// Data Validation: Implement validation checks for data consistency and integrity.
+// Referential Integrity: Set up constraints to maintain data integrity.
+// Audit Trails: Consider logging changes to maintain an audit trail.
+// Scalability: Design for handling a large number of reservations and resources efficiently.
+
+// Implement the logic to handle the edge cases within your application code using the Superbase client and API.
+
+// Note: This code outlines the database schema and edge case considerations. You'll need to integrate this schema into your application's logic and implement the handling of edge cases according to your requirements.
